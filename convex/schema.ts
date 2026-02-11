fc44562619405b6295841d56fc5e3f3d84ab503a
prateekkumaroriginal <prateek.kumar.original@gmail.com>
2026-02-11 22:19:05 +0530

    reset

--- convex/schema.ts
+++
@@ -1,9 +0,0 @@
-import { defineSchema, defineTable } from "convex/server";
-import { v } from "convex/values";
-
-export default defineSchema({
-  boards: defineTable({
-    name: v.string(),
-    createdAt: v.number()
-  }).index("by_created_at", ["createdAt"])
-});
